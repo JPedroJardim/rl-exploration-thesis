94f87fe189546dcc3817247865929cdc37529b6c
Pedro Jardim <pedro_jardim@hotmail.com>
2023-11-28 09:12:53 +0000

    adapted for multigpu env

--- src/dqn.py
+++ src/dqn.py
@@ -149,7 +149,11 @@ if __name__ == "__main__":
     print('Prepared environment.')
 
     # if GPU is to be used
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.device_count() > 1:
+        # is in bath cloud (ogg)
+        device = torch.device("cuda:5")
+    else:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
     Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
