7192df42ce97f4cd65cab6b45d0e8626c405c405
JPedroJardim <pedromonizjardim@gmail.com>
2024-02-08 14:20:07 +0000

    stable version

--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
 FROM pytorch/pytorch:2.0.1-cuda11.7-cudnn8-runtime
-RUN apt-get update && apt-get install -y  build-essential
+RUN apt-get update && apt-get install -y build-essential
 RUN pip3 install gymnasium==0.28.1
 RUN pip3 install "gymnasium[atari, accept-rom-license]"
 RUN pip3 install gym==0.26.2
--- src/dqn.py
+++ src/dqn.py
@@ -153,7 +153,15 @@ if __name__ == "__main__":
         # is in bath cloud (ogg)
     #    device = torch.device("cuda:5")
     #else:
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.is_available():
+        device_spec = "cuda"
+    elif torch.backends.mps.is_available():
+        device_spec = "mps"
+    else:
+        device_spec = "cpu"
+
+    print(f"Using device {device_spec}")
+    device = torch.device(device_spec)
     
     Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
